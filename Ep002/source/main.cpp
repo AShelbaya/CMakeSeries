1b47b2bb79e37bb4a306198534797ddc2b385aa2
Ahmad Shelbaya <aashelbaya@gmail.com>
2025-07-20 02:30:30 -0500

    Episode2

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <concepts>
+
+template<typename T>
+requires std::integral<T>
+T add(T a, T b) {
+    return a + b;
+}   
+
+int main() {
+    int x = 7, y = 5;
+	std::cout << "Hello from C++20 with CMake on Linux/Windows!" << std::endl;
+    std::cout << "Sum: " << add(x, y) << std::endl; // Valid usage with integral types
+
+    // Uncommenting the following line will cause a compilation error
+    // because std::string does not satisfy the std::integral concept.
+    // std::string str1 = "Hello, ", str2 = "World!";
+    // std::cout << "Concatenation: " << add(str1, str2) << std::endl;
+
+    return 0;
+}
